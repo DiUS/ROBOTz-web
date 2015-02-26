09b67a1169cf64cf5baaed3bbbaf0b1295e1d3d2
Ben Gummer <bengummer@gmail.com>
2015-02-26 11:08:32 +1100

    Submitting commands to Node backend

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'jade');
 
@@ -10,4 +13,12 @@ app.get('/', function (request, response) {
   response.render('index');
 });
 
+app.post('/commands', function(request, response) {
+  console.log(request.body);
+  response.json({
+    ok: true
+  });
+});
+
 app.listen(3000);
+console.log('ROBOTz-web running on port 3000');
--- public/robotz.js
+++ public/robotz.js
@@ -5,20 +5,70 @@ var backwardButton = document.querySelector('.backward');
 var turnLeftButton = document.querySelector('.turn-left');
 var turnRightButton = document.querySelector('.turn-right');
 
-var commandList = document.querySelector('.command-list textarea');
+var commandArea = document.querySelector('.command-list textarea');
+
+var submitButton = document.querySelector('.execute-commands button');
 
 forwardButton.addEventListener('click', function() {
-  commandList.value = commandList.value + 'Forward\n';
+  commandArea.value = commandArea.value + 'Forward\n';
 });
 
 backwardButton.addEventListener('click', function() {
-  commandList.value = commandList.value + 'Backward\n';
+  commandArea.value = commandArea.value + 'Backward\n';
 });
 
 turnLeftButton.addEventListener('click', function() {
-  commandList.value = commandList.value + 'Turn Left\n';
+  commandArea.value = commandArea.value + 'Turn Left\n';
 });
 
 turnRightButton.addEventListener('click', function() {
-  commandList.value = commandList.value + 'Turn Right\n';
+  commandArea.value = commandArea.value + 'Turn Right\n';
+});
+
+function httpRequest(onSucess, onError) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        onSucess(request.responseText);
+      }
+      else {
+        onError();
+      }
+    }
+  };
+  return request;
+}
+
+var http = {
+  post: function(data, onSuccess, onError) {
+    var request = httpRequest(onSuccess, onError);
+    request.open('POST', '/commands', true);
+    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    request.setRequestHeader('Content-type',     'application/json')
+    request.setRequestHeader('Accept',           'application/json')
+    request.send(JSON.stringify(data));
+  }
+};
+
+submitButton.addEventListener('click', function() {
+  submitButton.disabled = true;
+  var oldSubmitButtonValue = submitButton.innerText;
+  submitButton.innerText = 'Moving...';
+  var commandList = commandArea.value.split('\n');
+  commandList.pop();
+
+  var successFunction = function(responseText) {
+    submitButton.disabled = false;
+    document.querySelector('.command-result').innerText = 'Command sent!';
+    document.querySelector('.command-result').style.color = 'green';
+    submitButton.innerText = oldSubmitButtonValue;
+  };
+
+  http.post(commandList, successFunction, function() {
+    submitButton.disabled = false;
+    document.querySelector('.command-result').innerText = 'Error!';
+    document.querySelector('.command-result').style.color = 'red';
+    submitButton.innerText = oldSubmitButtonValue;
+  });
 });
