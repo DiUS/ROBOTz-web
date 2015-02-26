d145faf5ed43179074b7e59d3685e133ffdb140e
Josh Male <jmale.home@gmail.com>
2015-02-26 13:43:12 +1100

    changed the robot commands to go thru HTTP

--- models/robot.js
+++ models/robot.js
@@ -2,41 +2,32 @@
 
 module.exports = Robot;
 
+var request = require('sync-request');
+
 function Robot() {
-
-  // TOOD - uncomment the serial port code below when we're
-  // ready to talk to the arduino
-
-  // var serialPortName = "/dev/tty.usbmodem1411";
-  // var SerialPort     = require('serialport').SerialPort;
-  // var serialPort     = new SerialPort(serialPortName);
-
-  var serialPort = {
-    write: function(some_string, callback) {
-      console.log("writing %s to the serial port", some_string);
-    }
-  }
 
   return {
     move_forwards: function() {
-      serialPort.write('Move Forwards?', serialPortCallback);
+      sendCommand('forward');
     },
 
     move_backwards: function() {
-      serialPort.write('Move Backwards?', serialPortCallback);
+      sendCommand('backward');
     },
 
     turn_left: function() {
-      serialPort.write('Turn Left?', serialPortCallback);
+      sendCommand('left');
     },
 
     turn_right: function() {
-      serialPort.write('Turn Right?', serialPortCallback);
+      sendCommand('right');
     }
   };
 
-  function serialPortCallback(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
+  function sendCommand(command) {
+    var response = request('GET', 'http://localhost:4000/robotz/' + command);
+
+    console.log("response status: %s", response.getStatus());
+    console.log(response.getBody());
   }
 }
